79b837fb28b91b9e2c2203ef02594cc88902209a
raflikharismaa <raflikharisma29@webmail.umm.ac.id>
2024-04-05 15:44:42 +0700

    credential issues

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
-    secret: "rafliganteng",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
   })
